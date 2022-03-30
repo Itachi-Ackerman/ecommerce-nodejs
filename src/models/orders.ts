1c36720a24f6886788471c8ce658e390c257f1d4
Itachi <me@subhayubhattacharjee.com>
2022-03-30 12:51:22 +0530

    bug fix

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -2,40 +2,44 @@
  * @info schema for orders
  */
 
- import { boolean } from "joi";
+import { boolean } from "joi";
 import { Schema, model } from "mongoose";
 import { IProduct } from "./products";
 import { ISeller } from "./sellers";
 import { IUser } from "./users";
- 
- export interface IOrder {
-     _id: string;
-     user: string | IUser;
-     product: string | IProduct;
-     orderDate: string;
-     quantity: number;
-     delivered: boolean;
- }
- 
- const schema = new Schema({
-     user: {
-         type: Schema.Types.ObjectId,
-         ref: "users",
-         required: true
-     },
-     product: {
-         type: Schema.Types.ObjectId,
-         ref: "products",
-         required: true
-     },
-     orderDate: {
+
+export interface IOrder {
+    _id: string;
+    user: string | IUser;
+    product: string | IProduct;
+    orderDate: string;
+    quantity: number;
+    delivered: boolean;
+}
+
+const schema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    },
+    product: {
+        type: Schema.Types.ObjectId,
+        ref: "products",
+        required: true
+    },
+    orderDate: {
         type: String,
         required: true
-     },
-     delivered: {
-         type: Boolean,
-         required: true
-     },
- });
- 
- export default model<IOrder>("orders", schema);
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    delivered: {
+        type: Boolean,
+        required: true
+    },
+});
+
+export default model<IOrder>("orders", schema);
