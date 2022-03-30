17a4b2f8750f440359df58e68d212826dc1d73e2
Itachi <me@subhayubhattacharjee.com>
2022-03-30 12:23:00 +0530

    bug fix

--- src/controller/sellers.ts
+++ src/controller/sellers.ts
@@ -378,7 +378,7 @@ export default class CtrlSeller {
                     }
                 },
                 {
-                    $unwind: "product"
+                    $unwind: "$product"
                 },
                 match1,
                 {
